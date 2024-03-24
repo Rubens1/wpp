fed47c145b58ec272cd50f9a940462c18bcf6517
Rubens Nogueira <rubens.jesus1997@gmail.com>
2024-03-24 19:11:40 -0300

    729F240DAECF56C21B886972BB3C812467A71ABFF670C664A4EF0395D2E8C99DA172A6A32FA770DBCD23A3ED172C8874052F709ADF6BB7FF37782199F7B825FF
    729F240DAECF56C21B886972BB3C812467A71ABFF670C664A4EF0395D2E8C99DA172A6A32FA770DBCD23A3ED172C8874052F709ADF6BB7FF37782199F7B825FF

---
+++ app.js
@@ -0,0 +1,50 @@
+const fs = require('fs');
+const wppconnect = require('@wppconnect-team/wppconnect');
+
+wppconnect
+  .create({
+    session: 'sessionName',
+    catchQR: (base64Qr, asciiQR) => {
+      console.log(asciiQR); // Optional to log the QR in the terminal
+      var matches = base64Qr.match(/^data:([A-Za-z-+\/]+);base64,(.+)$/),
+        response = {};
+
+      if (matches.length !== 3) {
+        return new Error('Invalid input string');
+      }
+      response.type = matches[1];
+      response.data = new Buffer.from(matches[2], 'base64');
+
+      var imageBuffer = response;
+      require('fs').writeFile(
+        'out.png',
+        imageBuffer['data'],
+        'binary',
+        function (err) {
+          if (err != null) {
+            console.log(err);
+          }
+        }
+      );
+    },
+    logQR: false,
+  })
+  .then((client) => start(client))
+  .catch((error) => console.log(error));
+
+
+  
+  function start(client) {
+    client.onMessage((message) => {
+      if (message.body === 'Hello') {
+        client
+          .sendText(message.from, 'Hello, how I may help you?')
+          .then((result) => {
+            console.log('Result: ', result); //return object success
+          })
+          .catch((erro) => {
+            console.error('Error when sending: ', erro); //return object error
+          });
+      }
+    });
+  }
